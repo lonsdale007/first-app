4ebabad36dca7e8b62ca69e9ec413257a9a2090f
lonsdale007 <gor.ros@yandex.ru>
2022-11-17 16:39:13 +0300

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="Header">
+            <a href="#">Home</a>
+            <a href="#">News</a>
+            <a href="#">FAQ</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import './Main.css';
+
+function Main() {
+    return (
+        <div className="Main">
+            <ul>
+                <li>First</li>
+                <li>Second</li>
+                <li>Third</li>
+                <li>Fourth</li>
+                <li>Fifth</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Main;
