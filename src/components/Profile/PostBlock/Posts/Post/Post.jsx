03c054870fcfe5fedc86ffd97b66041d50a3c768
lonsdale007 <gor.ros@yandex.ru>
2022-11-19 13:28:11 +0300

    Подкорректировал расположение контента в посте

--- src/components/Profile/PostBlock/Posts/Post/Post.jsx
+++ src/components/Profile/PostBlock/Posts/Post/Post.jsx
@@ -3,16 +3,18 @@ import p from './Post.module.css';
 function Post() {
     return (
         <div className={p.post}>
-            <div className={p.avatar}>
-                <img src={require('../../../../../img/avatar.jpg')} alt={''}/>
-            </div>
-            <div className={p.text}>
-                Text...
+            <div className={p.content}>
+                <div className={p.avatar}>
+                    <img src={require('../../../../../img/avatar.jpg')} alt={''}/>
+                </div>
+                <div className={p.text}>
+                    Text...
+                </div>
             </div>
             <div className={p.like}>
                 <img src={require('../../../../../img/unliked.png')}
-                onMouseOver={el => (el.currentTarget.src = require('../../../../../img/unliked_hover.png'))}
-                onMouseOut={el => (el.currentTarget.src = require('../../../../../img/unliked.png'))} alt={''}/>
+                     onMouseOver={el => (el.currentTarget.src = require('../../../../../img/unliked_hover.png'))}
+                     onMouseOut={el => (el.currentTarget.src = require('../../../../../img/unliked.png'))} alt={''}/>
             </div>
         </div>
     )
