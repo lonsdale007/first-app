96183adfe40a74f98c4af4230f86459b944ff06e
lonsdale007 <gor.ros@yandex.ru>
2022-11-20 21:10:50 +0300

    Вставил элементы навбара через тег, добавил картинку и ссылку для роутов

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,15 @@
 import n from './Nav.module.css';
+import NavElement from "./NavElement/NavElement";
 
 const Nav = () => {
     return (
         <nav className={n.nav_bar}>
-            <div>
-                <a>Profile</a>
-            </div>
-            <div>
-                <a>Messages</a>
-            </div>
-            <div>
-                <a>News</a>
-            </div>
-            <div>
-                <a>Music</a>
-            </div>
-            <div>
-                <a>Settings</a>
-            </div>
+            <NavElement pic={require('../../img/profile.png')} name={'Profile'} link={['/profile']}/>
+            <NavElement pic={require('../../img/messages.png')} name={'Messages'} link={['/messages']}/>
+            <NavElement pic={require('../../img/news.png')} name={'News'} link={['/news']}/>
+            <NavElement pic={require('../../img/music.png')} name={'Music'} link={['/music']}/>
+            <br/>
+            <NavElement pic={require('../../img/settings.png')} name={'Settings'} link={['/settings']}/>
         </nav>
     )
 }
