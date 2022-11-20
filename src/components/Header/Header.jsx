09dcbf83ae65d333c2fa33b00b52e9aab631d900
lonsdale007 <gor.ros@yandex.ru>
2022-11-20 21:09:10 +0300

    Обернул контент хедера в новый див

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,11 @@ import h from './Header.module.css';
 const Header = () => {
     return (
         <header className={h.header}>
-            <img src={logo} alt={''}/>
-            <div className={h.app_name}>
-                Li-<text id={'right_part_name'}>ON</text>
+            <div className={h.content}>
+                <img src={logo} alt={''}/>
+                <div className={h.app_name}>
+                    Li-<text id={'right_part_name'}>ON</text>
+                </div>
             </div>
         </header>
     )
