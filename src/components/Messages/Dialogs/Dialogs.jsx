3e45067dae4be3deedc6d700297453224da8bbfd
lonsdale007 <gor.ros@yandex.ru>
2022-11-20 21:08:36 +0300

    Добавил один диалог в список диалогов через тег

--- src/components/Messages/Dialogs/Dialogs.jsx
+++ src/components/Messages/Dialogs/Dialogs.jsx
@@ -1,11 +1,13 @@
 import d from './Dialogs.module.css'
 import SearchBlock from "./SearchBlock/SearchBlock";
+import Dialog from "./Dialog/Dialog";
 
 
 const Dialogs = (props) => {
     return (
         <div className={d.dialogs}>
             <SearchBlock/>
+            <Dialog name={'Elyash'} text={'Hi'}/>
         </div>
     )
 }
