ba407e963f79c3a295df9a84cfb48f970188e90d
lonsdale007 <gor.ros@yandex.ru>
2022-11-20 14:35:47 +0300

    Создал компоненту и стили вкладки сообщений

---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,14 @@
+import m from './Messages.module.css';
+import Dialogs from './Dialogs/Dialogs';
+import Chat from "./Chat/Chat";
+
+const Messages = (props) => {
+    return (
+        <div className={m.messages}>
+            <Dialogs/>
+            <Chat/>
+        </div>
+    )
+}
+
+export default Messages;
