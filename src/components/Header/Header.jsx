bd9a7386d2af5b448f75f91a1eb35e7da5677666
lonsdale007 <gor.ros@yandex.ru>
2022-11-23 21:52:37 +0300

    Сделал хедер фиксированным

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className={h.header}>
             <div className={h.content}>
-                <img src={logo} alt={''}/>
+                <img className={h.header_img} src={logo} alt={''}/>
                 <div className={h.app_name}>
                     Li-<text id={'right_part_name'}>ON</text>
                 </div>
