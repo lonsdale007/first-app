ba231af5cce4fbeedf8608dc0c59b737f2de6704
lonsdale007 <gor.ros@yandex.ru>
2022-11-25 21:58:45 +0300

    Настроил отступы и размер лайка

--- src/components/Profile/PostBlock/Posts/Posts.jsx
+++ src/components/Profile/PostBlock/Posts/Posts.jsx
@@ -7,7 +7,7 @@ const Posts = () => {
         <div className={ps.posts}>
             <Post avatar={avatar} name={'Danila Artemov'} date={'23 ноя в 13:00'} text={'I\'m gay'} likes_count={'69'}/>
             <Post avatar={avatar} name={'Danila Artemov'} date={'17 ноя в 11:13'} text={'Wassup, m8?'} likes_count={'1'}/>
-            <Post avatar={avatar} name={'Danila Artemov'} date={'10 ноя в 15:24'} text={'Way of Samurai'} likes_count={'-3'}/>
+            <Post avatar={avatar} name={'Danila Artemov'} date={'10 ноя в 15:24'} text={'Way of Samurai'} likes_count={'3'}/>
         </div>
     )
 }
