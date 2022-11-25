7a3850218ef82aa94cb27222bcef4b47242d9f1f
lonsdale007 <gor.ros@yandex.ru>
2022-11-25 22:20:31 +0300

    Исправил передачу линка

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,12 +4,12 @@ import NavElement from "./NavElement/NavElement";
 const Nav = () => {
     return (
         <nav className={n.nav_bar}>
-            <NavElement pic={require('../../img/profile.png')} name={'Profile'} link={['/profile']}/>
-            <NavElement pic={require('../../img/messages.png')} name={'Messages'} link={['/messages']}/>
-            <NavElement pic={require('../../img/news.png')} name={'News'} link={['/news']}/>
-            <NavElement pic={require('../../img/music.png')} name={'Music'} link={['/music']}/>
+            <NavElement pic={require('../../img/profile.png')} name={'Profile'} link={'/profile'}/>
+            <NavElement pic={require('../../img/messages.png')} name={'Messages'} link={'/messages'}/>
+            <NavElement pic={require('../../img/news.png')} name={'News'} link={'/news'}/>
+            <NavElement pic={require('../../img/music.png')} name={'Music'} link={'/music'}/>
             <br/>
-            <NavElement pic={require('../../img/settings.png')} name={'Settings'} link={['/settings']}/>
+            <NavElement pic={require('../../img/settings.png')} name={'Settings'} link={'/settings'}/>
         </nav>
     )
 }
