35eaa0d42ac9b81bd8582cbc09de2f3672ca83ac
lonsdale007 <gor.ros@yandex.ru>
2022-11-26 22:49:43 +0300

    удалил альт

--- src/components/Profile/ProfileCover/ProfileCover.jsx
+++ src/components/Profile/ProfileCover/ProfileCover.jsx
@@ -4,7 +4,7 @@ import cover from '../../../img/cover.jpg';
 const ProfileCover = () => {
     return (
         <div className={p.cover}>
-            <img src={cover} alt={'profile-cover'}/>
+            <img src={cover} alt={''}/>
         </div>
     )
 }
