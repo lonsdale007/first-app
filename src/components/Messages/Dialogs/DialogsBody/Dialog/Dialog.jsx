fd11de765740d7cc41651a90f76a98ac48345d8e
lonsdale007 <gor.ros@yandex.ru>
2022-11-26 22:49:11 +0300

    получил аватар через пропс

--- src/components/Messages/Dialogs/DialogsBody/Dialog/Dialog.jsx
+++ src/components/Messages/Dialogs/DialogsBody/Dialog/Dialog.jsx
@@ -4,7 +4,7 @@ import {NavLink} from "react-router-dom";
 const Dialog = (props) => {
     return (
         <NavLink exact to={'/messages/' + props.id} className={({ isActive }) => d.dialog + (isActive ? ' ' + d.active : '')}>
-            <img className={d.avatar} src={require('../../../../../img/Elya.jpg')} alt={''}/>
+            <img className={d.avatar} src={props.avatar} alt={''}/>
             <span className={d.name}>{props.name}</span>
             <span className={d.text}>{props.text}</span>
         </NavLink>
