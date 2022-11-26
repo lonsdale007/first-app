501a46611619f032993945758f27b1a22b965c66
lonsdale007 <gor.ros@yandex.ru>
2022-11-26 22:26:45 +0300

    изменил имя кнопки публикации поста и настроил цвет

--- src/components/Profile/PostBlock/NewPost/NewPost.jsx
+++ src/components/Profile/PostBlock/NewPost/NewPost.jsx
@@ -4,7 +4,7 @@ const NewPost = () => {
     return (
         <div className={np.new_post}>
             <textarea className={np.input_field} placeholder={'What\'s new?'}/>
-            <button className={np.submit} type={'submit'}>Post</button>
+            <button className={np.send_post}>Post</button>
         </div>
     )
 }
