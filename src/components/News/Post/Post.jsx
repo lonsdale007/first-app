e3daf59981d8bd7cb9d4ceb72dd93a08eafbd221
lonsdale007 <gor.ros@yandex.ru>
2022-11-26 22:27:45 +0300

    разбил компоненту поста на подкомпоненты: шапка поста и контент

--- src/components/News/Post/Post.jsx
+++ src/components/News/Post/Post.jsx
@@ -1,19 +1,12 @@
 import np from './Post.module.css';
+import PostHeader from "./PostHeader/PostHeader";
+import PostContent from "./PostContent/PostContent";
 
 const Post = (props) => {
     return (
         <div className={np.post}>
-            <img className={np.avatar} src={props.avatar} alt={''}/>
-            <span className={np.name}>{props.name}</span>
-            <span className={np.date}>{props.date}</span>
-            <div className={np.text}>{props.text}</div>
-            <img className={np.picture} src={props.pic} alt={''}/>
-            <div className={np.like}>
-                <img src={require('../../../img/unliked.png')}
-                     onMouseOver={el => (el.currentTarget.src = require('../../../img/unliked_hover.png'))}
-                     onMouseOut={el => (el.currentTarget.src = require('../../../img/unliked.png'))} alt={''}/>
-                <span>{props.likes_count}</span>
-            </div>
+            <PostHeader avatar={props.avatar} name={props.name} date={props.date}/>
+            <PostContent text={props.text} pic={props.pic} likes_count={props.likes_count}/>
         </div>
     )
 }
