e201b0e1a70f668230649c4af2493dcd4cf5fe81
lonsdale007 <gor.ros@yandex.ru>
2022-11-26 22:54:31 +0300

    импортировал аву группы

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,12 +1,11 @@
 import n from './News.module.css';
 import Post from "./Post/Post";
 import NewPost from "../Profile/PostBlock/NewPost/NewPost";
-import avatar from 'https://sun6-20.userapi.com/s/v1/ig2/a_mAKJuMJIEUj0R2iu6YrA8QH0-HvMCT3zqZWDrVfrv8X3uZLzNIN2Z9HABXPCU_YSnp0XZvDXKeT2EsnsL8PcHf.jpg?size=1150x1150&quality=96&crop=0,0,1150,1150&ava=1';
-
+import group_avatar from'../../img/group_avatar.jpg';
 const News = (props) => {
 
     let newsData = [
-        {avatar: avatar, name: 'Public group', date: '20 ноя в 19:13', text: 'Great sunset', pic: 'https://images.alphacoders.com/852/thumb-1920-852592.jpg', likes_count: 5}
+        {avatar: group_avatar, name: 'Public group', date: '20 ноя в 19:13', text: 'Great sunset', pic: 'https://images.alphacoders.com/852/thumb-1920-852592.jpg', likes_count: 5}
     ]
 
     return (
