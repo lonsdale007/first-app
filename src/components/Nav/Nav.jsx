73a48f7ecef0506a08ecb909d47add87bfea10c3
lonsdale007 <gor.ros@yandex.ru>
2022-11-26 22:25:20 +0300

    импортировал иконки навбара

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,20 @@
 import n from './Nav.module.css';
 import NavElement from "./NavElement/NavElement";
+import profile_icon from '../../img/profile.svg';
+import messages_icon from '../../img/messages.svg';
+import news_icon from '../../img/news.svg';
+import music_icon from '../../img/music.svg';
+import settings_icon from '../../img/settings.svg';
 
 const Nav = () => {
     return (
         <nav className={n.nav_bar}>
-            <NavElement pic={require('../../img/profile.png')} name={'Profile'} link={'/profile'}/>
-            <NavElement pic={require('../../img/messages.png')} name={'Messages'} link={'/messages'}/>
-            <NavElement pic={require('../../img/news.png')} name={'News'} link={'/news'}/>
-            <NavElement pic={require('../../img/music.png')} name={'Music'} link={'/music'}/>
+            <NavElement pic={profile_icon} name={'Profile'} link={'/profile'}/>
+            <NavElement pic={messages_icon} name={'Messages'} link={'/messages'}/>
+            <NavElement pic={news_icon} name={'News'} link={'/news'}/>
+            <NavElement pic={music_icon} name={'Music'} link={'/music'}/>
             <br/>
-            <NavElement pic={require('../../img/settings.png')} name={'Settings'} link={'/settings'}/>
+            <NavElement pic={settings_icon} name={'Settings'} link={'/settings'}/>
         </nav>
     )
 }
