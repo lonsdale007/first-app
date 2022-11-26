f112ea6dfd7300fe9aaa5b177a49f323ba09dd06
lonsdale007 <gor.ros@yandex.ru>
2022-11-26 19:20:59 +0300

    создал компоненту для элемента сообщения и стили для нее

---
+++ src/components/Messages/Chat/ChatBody/Message/Message.jsx
@@ -0,0 +1,16 @@
+import m from './Message.module.css';
+import d from "../../../Dialogs/DialogsBlock/Dialog/Dialog.module.css";
+
+const Message = (props) => {
+
+    let isIncoming = props.isIncoming;
+
+    return (
+        <div className={m.message + (isIncoming ? ' ' + m.incoming : ' ' + m.outgoing)}>
+            <div className={m.text}>{props.text}</div>
+            <span className={m.date}>{props.date}</span>
+        </div>
+    )
+}
+
+export default Message;
