e7c113d68fe14981eaf41606b4f4f7067ece67d8
lonsdale007 <gor.ros@yandex.ru>
2022-11-26 19:13:28 +0300

    Разбил компоненту чат на подкомпоненты и добавил грид для их отображения

--- src/components/Messages/Chat/Chat.jsx
+++ src/components/Messages/Chat/Chat.jsx
@@ -1,9 +1,15 @@
 import ch from './Chat.module.css';
+import ChatHeader from "./ChatHeader/ChatHeader";
+import avatar from '../../../img/Elya.jpg';
+import ChatBody from "./ChatBody/ChatBody";
+import ChatInput from "./ChatInput/ChatInput";
 
 const Chat = (props) => {
     return (
         <div className={ch.chat}>
-
+            <ChatHeader name={'Elyash'} avatar={avatar}/>
+            <ChatBody/>
+            <ChatInput/>
         </div>
     )
 }
