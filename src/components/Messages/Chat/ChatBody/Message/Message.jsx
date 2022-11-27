6dab34bb976afd607af05c87410bc7e4e10be9a3
lonsdale007 <gor.ros@yandex.ru>
2022-11-27 17:48:23 +0300

    убрал переменную для проверки исходящего/входящего сообщения, вставил сразу через пропсы

--- src/components/Messages/Chat/ChatBody/Message/Message.jsx
+++ src/components/Messages/Chat/ChatBody/Message/Message.jsx
@@ -2,10 +2,8 @@ import m from './Message.module.css';
 
 const Message = (props) => {
 
-    let isIncoming = props.isIncoming;
-
     return (
-        <div className={m.message + (isIncoming ? ' ' + m.incoming : ' ' + m.outgoing)}>
+        <div className={m.message + (props.isIncoming ? ' ' + m.incoming : ' ' + m.outgoing)}>
             <div className={m.text}>{props.text}</div>
             <span className={m.date}>{props.date}</span>
         </div>
