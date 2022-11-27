6b95c60ed68f0ca984cf5b751667204643bea2c3
lonsdale007 <gor.ros@yandex.ru>
2022-11-27 17:44:36 +0300

    маппингом создал элементы сообщений

--- src/components/Messages/Chat/ChatBody/ChatBody.jsx
+++ src/components/Messages/Chat/ChatBody/ChatBody.jsx
@@ -1,15 +1,13 @@
 import cb from './ChatBody.module.css';
 import Message from "./Message/Message";
-import avatarElya from '../../../../img/Elya.jpg';
-import avatarMine from '../../../../img/avatar.jpg'
 
 const ChatBody = (props) => {
+
+    let messagesElements = props.messages.map( m => <Message avatar={m.avatar} date={m.date} text={m.text} isIncoming={m.isIncoming}/> );
+
     return (
         <div className={cb.chat}>
-            <Message avatar={avatarElya} date={'11:18'} text={'Hello'} isIncoming={true}/>
-            <Message avatar={avatarElya} date={'11:30'} text={'How are you?'} isIncoming={true}/>
-            <Message avatar={avatarElya} date={'11:31'} text={'What r u doin?'} isIncoming={true}/>
-            <Message avatar={avatarMine} date={'11:36'} text={'hi, im fine, wo ba u?'} isIncoming={false}/>
+            {messagesElements}
         </div>
     )
 }
