dd6753046651470c6f107087dc9680f62a89404f
lonsdale007 <gor.ros@yandex.ru>
2022-11-29 21:51:17 +0300

    перенес части кода для читаемости

--- src/components/Profile/PostBlock/MyPosts/MyPosts.jsx
+++ src/components/Profile/PostBlock/MyPosts/MyPosts.jsx
@@ -3,7 +3,9 @@ import Post from "../../../News/Post/Post";
 
 const MyPosts = (props) => {
 
-    let myPostsElements = props.myPosts.map( p => <Post avatar={p.avatar} name={p.name} date={p.date} text={p.text} pic={p.pic} likes_count={p.likes_count} key={p.id}/> ).reverse();
+    let myPostsElements = props.myPosts.map(
+        p => <Post avatar={p.avatar} name={p.name} date={p.date} text={p.text}
+                   pic={p.pic} likes_count={p.likes_count} key={p.id}/> ).reverse();
 
     return (
         <div className={ps.posts}>
