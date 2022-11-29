9ad9138d47147ef74d1ccd5236aaa8aa712862f8
lonsdale007 <gor.ros@yandex.ru>
2022-11-29 21:49:11 +0300

    убрал передачу данных и заменил обычные компоненты на контейнерные

--- src/components/Profile/PostBlock/PostBlock.jsx
+++ src/components/Profile/PostBlock/PostBlock.jsx
@@ -1,12 +1,12 @@
 import pb from './PostBlock.module.css';
-import MyPosts from "./MyPosts/MyPosts";
-import NewPost from "./NewPost/NewPost";
+import NewPostContainer from "./NewPost/NewPostContainer";
+import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
 const PostBlock = (props) => {
     return (
         <div className={pb.post_block}>
-            <NewPost newPostText={props.newPostText} dispatch={props.dispatch}/>
-            <MyPosts myPosts={props.myPosts}/>
+            <NewPostContainer/>
+            <MyPostsContainer/>
         </div>
     )
 }
