8d124f8177eee2c1fb05fc50626ee88e5d810c0e
lonsdale007 <gor.ros@yandex.ru>
2022-11-29 20:02:43 +0300

    создал initialState для редусеров

--- src/redux/messagesReducer.js
+++ src/redux/messagesReducer.js
@@ -1,7 +1,31 @@
+import avaElya from "../img/Elya.jpg";
+import avaDima from "../img/Dima.jpg";
+import avaVanya from "../img/Vanya.jpg";
+import avaEgor from "../img/Egor.jpg";
+
 const SEND_MESSAGE = 'SEND-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
-const messagesReducer = (state, action) => {
+let initState = {
+    dialogs: [
+        { id: 1, name: 'Elvira Shurygina', avatar: avaElya, text: 'hi, im fine, wo ba u?' },
+        { id: 2, name: 'Dmitry Chernikov', avatar: avaDima, text: 'hello from KZ' },
+        { id: 3, name: 'Ivan Bogomolov', avatar: avaVanya, text: 'read about js on mdn' },
+        { id: 4, name: 'Egor Emelyanov', avatar: avaEgor, text: 'go cs' },
+    ],
+    messages: [
+        { id_dialog: 1, id_message: 1, date: '11:18', text: 'Hello', isIncoming: true },
+        { id_dialog: 1, id_message: 2, date: '11:30', text: 'How are you?', isIncoming: true },
+        { id_dialog: 1, id_message: 3, date: '11:31', text: 'hat r u doin?', isIncoming: true },
+        { id_dialog: 1, id_message: 4, date: '11:36', text: 'hi, im fine, wo ba u?', isIncoming: false },
+        { id_dialog: 2, id_message: 1, date: '12:25', text: 'hello from KZ', isIncoming: true },
+        { id_dialog: 3, id_message: 1, date: '16:13', text: 'read about js on mdn', isIncoming: true },
+        { id_dialog: 4, id_message: 1, date: '15:14', text: 'go cs', isIncoming: true },
+    ],
+    newMessageText: '',
+}
+
+const messagesReducer = (state = initState, action) => {
 
     let currentDateTime = new Date();
     let currentHours = currentDateTime.getHours();
