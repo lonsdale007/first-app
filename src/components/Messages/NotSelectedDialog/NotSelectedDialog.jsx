584e89b64638ad1723e79bab06e4db0711de61f3
lonsdale007 <gor.ros@yandex.ru>
2022-11-29 17:01:55 +0300

    создал компоненту для отображения пустого (не выбранного) диалога и стили для нее

---
+++ src/components/Messages/NotSelectedDialog/NotSelectedDialog.jsx
@@ -0,0 +1,10 @@
+import nsd from './NotSelectedDialog.module.css';
+
+const NotSelectedDialog = (props) => {
+    return (
+        <div className={nsd.page}>
+            Select a chat
+        </div>
+    )
+}
+export default NotSelectedDialog;
