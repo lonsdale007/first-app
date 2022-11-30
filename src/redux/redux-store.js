c905d9747bbb7f75086ee3e95dd9ba8cf6e18d0b
lonsdale007 <gor.ros@yandex.ru>
2022-11-30 20:01:37 +0300

    добавил newsReducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,10 +1,12 @@
 import {combineReducers, legacy_createStore} from "redux";
 import profileReducer from "./profileReducer";
 import messagesReducer from "./messagesReducer";
+import newsReducer from "./newsReducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: messagesReducer,
+    newsPage: newsReducer,
 })
 
 let store = legacy_createStore(reducers);
