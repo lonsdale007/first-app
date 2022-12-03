ba1568ff385049df4b24919831299a39b730591a
lonsdale007 <gor.ros@yandex.ru>
2022-12-03 11:41:28 +0300

    вывел аватар в отдельный компонент и сделал для него стили

---
+++ src/components/common/Avatar/Avatar.jsx
@@ -0,0 +1,25 @@
+import a from './Avatar.module.css'
+
+const Avatar = (props) => {
+
+    let avatarSize = (size) => {
+        switch (size) {
+            case 'small':
+                return a.avatar_s;
+            case 'medium':
+                return a.avatar_m;
+            case 'large':
+                return a.avatar_s;
+            case 'profile':
+                return a.avatar_profile;
+            default:
+                return a.avatar_full;
+        }
+    };
+
+    return (
+        <img className={`${a.avatar} ${avatarSize(props.size)}`} src={props.avatar} alt={'user_avatar'}/>
+    )
+}
+
+export default Avatar;
