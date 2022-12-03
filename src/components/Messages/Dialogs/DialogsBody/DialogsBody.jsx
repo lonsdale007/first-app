869d65dcbfab9c546334a373f2a75b2267a7a7a6
lonsdale007 <gor.ros@yandex.ru>
2022-12-03 11:42:38 +0300

    перенес ключ на начало, переименовал id в dialog_id

--- src/components/Messages/Dialogs/DialogsBody/DialogsBody.jsx
+++ src/components/Messages/Dialogs/DialogsBody/DialogsBody.jsx
@@ -3,7 +3,7 @@ import Dialog from "./Dialog/Dialog";
 
 const DialogsBody = (props) => {
 
-    let dialogsElements = props.dialogs.map( d => <Dialog id={d.id} name={d.name} avatar={d.avatar} text={d.text} key={d.id}/> );
+    let dialogsElements = props.dialogs.map( d => <Dialog key={d.dialog_id} id={d.dialog_id} name={d.name} avatar={d.avatar} text={d.text}/> );
 
     return (
         <div className={db.dialogs}>
