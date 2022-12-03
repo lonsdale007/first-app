6e9a82ec2f660eed5074b6b9136097f32ced44bf
lonsdale007 <gor.ros@yandex.ru>
2022-12-03 19:40:07 +0300

    добавил usersPage

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import {combineReducers, legacy_createStore} from "redux";
 import profileReducer from "./profileReducer";
 import messagesReducer from "./messagesReducer";
 import newsReducer from "./newsReducer";
+import usersReducer from "./usersReducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: messagesReducer,
     newsPage: newsReducer,
+    usersPage: usersReducer,
 })
 
 let store = legacy_createStore(reducers);
