c25981b8d1ddd7716a62b294b98fe7391120ac5e
lonsdale007 <gor.ros@yandex.ru>
2022-12-03 11:40:33 +0300

    мой аватар заменил на дефолтный

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,15 +1,15 @@
-import avaMine from "../img/avatar.jpg";
+import user_avatar from "../img/user_avatar.svg";
 import lionScreen from "../img/lionAppScreen.jpg";
 
 const CREATE_POST = 'CREATE-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-let initState = {
+const initState = {
     myPosts: [
-        {id: 1, avatar: avaMine, name: 'Danila Artemov', date: '10 Nov at 15:24', text: 'Way of Samurai', likes_count: 3},
-        {id: 2, avatar: avaMine, name: 'Danila Artemov', date: '17 Nov at 11:13', text: 'Wassup, m8?', likes_count: 1},
-        {id: 4, avatar: avaMine, name: 'Danila Artemov', date: '23 Nov at 13:00', text: 'I\'m gay', likes_count: 69},
-        {id: 5, avatar: avaMine, name: 'Danila Artemov', date: 'today at 12:51', text: 'new post', pic: lionScreen, likes_count: 2},
+        {id: 1, avatar: user_avatar, name: 'Danila Artemov', date: '10 Nov at 15:24', text: 'Way of Samurai', likes_count: 3},
+        {id: 2, avatar: user_avatar, name: 'Danila Artemov', date: '17 Nov at 11:13', text: 'Wassup, m8?', likes_count: 1},
+        {id: 4, avatar: user_avatar, name: 'Danila Artemov', date: '23 Nov at 13:00', text: 'I\'m gay', likes_count: 69},
+        {id: 5, avatar: user_avatar, name: 'Danila Artemov', date: 'today at 12:51', text: 'new post', pic: lionScreen, likes_count: 2},
     ],
     newPostText: '',
 }
@@ -20,12 +20,12 @@ const profileReducer = (state = initState, action) => {
     let currentHours = currentDateTime.getHours();
     let currentMinutes = currentDateTime.getMinutes();
     let currentTime = `${currentHours < 10 ? '0' + currentHours : currentHours}:${currentMinutes < 10 ? '0' + currentMinutes : currentMinutes}`;
-    debugger
+
     switch (action.type) {
         case(CREATE_POST):
             let newPost = {
                 id: 7,
-                avatar: avaMine,
+                avatar: user_avatar,
                 name: 'Danila Artemov',
                 date: `today at ${currentTime}`,
                 text: state.newPostText,
