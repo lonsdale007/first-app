b57a7a684ee1c3f56de9fe1d1a3f8c59fabaa9aa
lonsdale007 <gor.ros@yandex.ru>
2022-12-03 11:40:28 +0300

    мой аватар заменил на дефолтный

--- src/redux/newsReducer.js
+++ src/redux/newsReducer.js
@@ -1,5 +1,5 @@
-import avaMine from "../img/avatar.jpg";
-import group_avatar from "../img/group_avatar.jpg";
+import user_avatar from "../img/user_avatar.svg";
+import group_avatar from "../img/group_avatar.svg";
 import sunset from "../img/sunset.jpg";
 import lionScreen from "../img/lionAppScreen.jpg";
 import nature from "../img/nature.jpg";
@@ -7,11 +7,11 @@ import nature from "../img/nature.jpg";
 
 let initState = {
     newsPosts: [
-        { id: 1, avatar: avaMine, name: 'Danila Artemov', date: '10 Nov at 15:24', text: 'Way of Samurai', likes_count: 3 },
-        { id: 2, avatar: avaMine, name: 'Danila Artemov', date: '17 Nov at 11:13', text: 'Wassup, m8?', likes_count: 1 },
+        { id: 1, avatar: user_avatar, name: 'Danila Artemov', date: '10 Nov at 15:24', text: 'Way of Samurai', likes_count: 3 },
+        { id: 2, avatar: user_avatar, name: 'Danila Artemov', date: '17 Nov at 11:13', text: 'Wassup, m8?', likes_count: 1 },
         { id: 3, avatar: group_avatar, name: 'Public group', date: '20 Nov at 19:13', text: 'Great sunset', pic: sunset, likes_count: 34 },
-        { id: 4, avatar: avaMine, name: 'Danila Artemov', date: '23 Nov at 13:00', text: 'I\'m gay', likes_count: 69 },
-        { id: 5, avatar: avaMine, name: 'Danila Artemov', date: 'today at 12:51', text: 'new post', pic: lionScreen, likes_count: 2 },
+        { id: 4, avatar: user_avatar, name: 'Danila Artemov', date: '23 Nov at 13:00', text: 'I\'m gay', likes_count: 69 },
+        { id: 5, avatar: user_avatar, name: 'Danila Artemov', date: 'today at 12:51', text: 'new post', pic: lionScreen, likes_count: 2 },
         { id: 6, avatar: group_avatar, name: 'Public group', date: 'today at 13:09', text: 'What a beautiful nature!', pic: nature, likes_count: 12 },
     ],
 }
