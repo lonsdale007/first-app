91ff72645477b39c689cf21fe22cf47d7b3a27ba
lonsdale007 <gor.ros@yandex.ru>
2022-12-03 19:39:02 +0300

    новый элемент в навбаре: Users

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import n from './Nav.module.css';
 import NavElement from "./NavElement/NavElement";
 import profile_icon from '../../img/profile.svg';
 import messages_icon from '../../img/messages.svg';
+import users_icon from '../../img/users.svg';
 import news_icon from '../../img/news.svg';
 import music_icon from '../../img/music.svg';
 import settings_icon from '../../img/settings.svg';
@@ -11,6 +12,7 @@ const Nav = () => {
         <nav className={n.nav_bar}>
             <NavElement pic={profile_icon} name={'Profile'} link={'/profile'}/>
             <NavElement pic={messages_icon} name={'Messages'} link={'/messages'}/>
+            <NavElement pic={users_icon} name={'Users'} link={'/users'}/>
             <NavElement pic={news_icon} name={'News'} link={'/news'}/>
             <NavElement pic={music_icon} name={'Music'} link={'/music'}/>
             <br/>
