96010dbb1e8dd9eba8a1cbcc5c8a703be7066744
lonsdale007 <gor.ros@yandex.ru>
2022-12-03 11:41:46 +0300

    удалил хардкод

--- src/components/Messages/Chat/Chat.jsx
+++ src/components/Messages/Chat/Chat.jsx
@@ -1,13 +1,12 @@
 import ch from './Chat.module.css';
 import ChatHeader from "./ChatHeader/ChatHeader";
-import avaElya from '../../../img/Elya.jpg';
 import ChatBodyContainer from "./ChatBody/ChatBodyContainer";
 import ChatInputContainer from "./ChatInput/ChatInputContainer";
 
 const Chat = (props) => {
     return (
         <div className={ch.chat}>
-            <ChatHeader name={'Elvira Shurygina'} avatar={avaElya}/>
+            <ChatHeader/>
             <ChatBodyContainer/>
             <ChatInputContainer/>
         </div>
