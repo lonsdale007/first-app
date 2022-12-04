cbc8f21e5ac8d3e322b7330a1fa7c776cc4e05af
lonsdale007 <gor.ros@yandex.ru>
2022-12-04 20:42:29 +0300

    получаю данные профиля юзера и вставляю

--- src/components/Profile/InfoBlock/InfoBlock.jsx
+++ src/components/Profile/InfoBlock/InfoBlock.jsx
@@ -1,18 +1,23 @@
 import styles from './InfoBlock.module.css';
 import Avatar from "../../common/Avatar/Avatar";
 import user_avatar from '../../../assets/images/user_avatar.svg'
+import Loader from "../../common/Loader/Loader";
 
-const InfoBlock = () => {
+const InfoBlock = (props) => {
+
+    if (!props.profile) {
+        return <Loader/>
+    }
     return (
         <div className={styles.block}>
             <div className={styles.avatar}>
-                <Avatar avatar={user_avatar} size={'profile'}/>
+                <Avatar avatar={props.profile.photos.large != null ? props.profile.photos.large : user_avatar} size={'profile'}/>
                 <button className={styles.update_avatar}>Update</button>
             </div>
             <div className={styles.description}>
-                <div className={styles.name}>Danila Artemov</div>
+                <div className={styles.name}>{props.profile.fullName}</div>
                 <br/>
-                <div>Date of birth:</div>
+                <div>About me: {props.profile.aboutMe}</div>
                 <div>City:</div>
                 <div>Education:</div>
                 <div>Web site:</div>
