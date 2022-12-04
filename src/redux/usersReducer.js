dcc3edf3d508a30f7e8c06c61e7ac9adab382303
lonsdale007 <gor.ros@yandex.ru>
2022-12-04 19:17:13 +0300

    добавил новый экшны (и крейторы для них): 1 Установить текущую страницу 2 Установить общее количество юзеров 3 Переключить значение свойства isFetching
    Переименовал userID в просто id

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,16 +1,21 @@
 const SET_USERS = 'SET_USERS';
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 const initState = {
     users: [],
-    totalCount: 4,
+    totalCount: 0,
+    pageSize: 10,
+    currentPage: 1,
+    isFetching: true,
 }
 
 const usersReducer = (state = initState, action) => {
     switch (action.type) {
         case SET_USERS:
-            debugger
             return {
                 ...state,
                 users: action.users,
@@ -19,7 +24,7 @@ const usersReducer = (state = initState, action) => {
             return {
                 ...state,
                 users: state.users.map(u => {
-                    if (u.userID === action.userID) {
+                    if (u.id === action.id) {
                         return {...u, followed: true}
                     }
                     return u;
@@ -29,19 +34,37 @@ const usersReducer = (state = initState, action) => {
             return {
                 ...state,
                 users: state.users.map(u => {
-                    if (u.userID === action.userID) {
+                    if (u.id === action.id) {
                         return {...u, followed: false}
                     }
                     return u;
                 }),
+            }
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.page,
+            }
+        case SET_TOTAL_USERS_COUNT:
+            return {
+                ...state,
+                totalCount: action.totalCount,
+            }
+        case TOGGLE_IS_FETCHING:
+            return {
+                ...state,
+                isFetching: action.isFetching,
             }
         default:
             return state
     }
 }
 
-export const setUsersAC = (users) => ({type: SET_USERS, users});
-export const followAC = (userID) => ({type: FOLLOW, userID});
-export const unfollowAC = (userID) => ({type: UNFOLLOW, userID});
+export const setUsers = (users) => ({type: SET_USERS, users});
+export const follow = (id) => ({type: FOLLOW, id});
+export const unfollow = (id) => ({type: UNFOLLOW, id});
+export const setCurrentPage = (page) => ({type: SET_CURRENT_PAGE, page});
+export const setTotalUsersCount = (totalCount) => ({type: SET_TOTAL_USERS_COUNT, totalCount});
+export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
 
 export default usersReducer;
