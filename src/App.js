91cbe5d9830e4a2938329bbf61206ac1bdb4fdfa
lonsdale007 <gor.ros@yandex.ru>
2022-12-04 20:43:02 +0300

    путь в profile не строгий, элемент теперь контейнерный компонент Profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-import Profile from "./components/Profile/Profile";
 import Messages from "./components/Messages/Messages";
 import {Route, Routes} from "react-router-dom";
 import NewsContainer from "./components/News/NewsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 const App = (props) => {
     return (
@@ -14,7 +14,7 @@ const App = (props) => {
             <div className={'app-wrapper'}>
                 <Nav/>
                 <Routes>
-                    <Route path={'/profile'} element={<Profile/>}/>
+                    <Route path={'/profile/*'} element={<ProfileContainer/>}/>
                     <Route path={'/messages/*'} element={<Messages/>}/>
                     <Route path={'/users/*'} element={<UsersContainer/>}/>
                     <Route path={'/news'} element={<NewsContainer/>}/>
