b38dbbcb771e79a2d6422e5a3f1410a00b8be81c
lonsdale007 <gor.ros@yandex.ru>
2022-12-04 19:21:39 +0300

    Переделал контейнерный компонент Users. Теперь этот файл содержит в себе сразу два контейнерных компонента. Один отвечает за общение с серверным API, другой за общение с Redux.
    Также зарефакторил мап диспатч

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,26 +1,86 @@
+import React from "react";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/usersReducer";
+import {
+    follow,
+    unfollow,
+    setCurrentPage,
+    setTotalUsersCount,
+    setUsers,
+    toggleIsFetching,
+} from "../../redux/usersReducer";
+import axios from "axios";
 import Users from "./Users";
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        this.props.toggleIsFetching(true);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials: true})
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+                this.props.toggleIsFetching(false);
+            })
+    }
+
+    onPageChanged = (page) => {
+        this.props.setCurrentPage(page);
+        this.props.toggleIsFetching(true);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.toggleIsFetching(false);
+            })
+    }
+    toNextPage = (page) => {
+        if (this.props.currentPage < 10) {
+            this.props.setCurrentPage(page+1);
+            this.props.toggleIsFetching(true);
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page+1}&count=${this.props.pageSize}`)
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                    this.props.toggleIsFetching(false);
+                })
+        }
+    }
+    toPrevPage = (page) => {
+        if (this.props.currentPage > 1) {
+            this.props.setCurrentPage(page-1);
+            this.props.toggleIsFetching(true);
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page-1}&count=${this.props.pageSize}`)
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                    this.props.toggleIsFetching(false);
+                })
+        }
+    }
+
+    render() {
+        return (
+            <Users
+                users={this.props.users}
+                totalCount={this.props.totalCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                toNextPage={this.toNextPage}
+                toPrevPage={this.toPrevPage}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+                isFetching={this.props.isFetching}
+            />
+        )
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
         totalCount: state.usersPage.totalCount,
+        pageSize: state.usersPage.pageSize,
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching,
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        setUsers: (users) => {
-            dispatch(setUsersAC(users));
-        },
-        follow: (userID) => {
-            dispatch(followAC(userID));
-        },
-        unfollow: (userID) => {
-            dispatch(unfollowAC(userID));
-        },
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, {setUsers, follow, unfollow, setCurrentPage, setTotalUsersCount, toggleIsFetching})(UsersContainer);
