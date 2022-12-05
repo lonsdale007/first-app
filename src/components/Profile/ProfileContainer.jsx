b2088c51127b38364c453a4ed7140b804e00b596
lonsdale007 <gor.ros@yandex.ru>
2022-12-05 20:36:11 +0300

    зарефакторил запрос с учетом получения айди юзера с адресной строки, добавил хедер в запрос с апи кейем моим

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,19 @@ import {connect} from "react-redux";
 import React from 'react';
 import axios from "axios";
 import {setUserProfile} from "../../redux/profileReducer";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
 
 class ProfileContainer extends React.Component{
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`, {withCredentials: true})
+        let userID = this.props.router.params.userID;
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userID}`,
+            {
+                withCredentials: true,
+                headers: {
+                    "API-KEY": "a7e14689-3433-40ab-b21b-cc7303eee722"
+                }
+            })
             .then(response => {
                 this.props.setUserProfile(response.data)
             })
@@ -24,4 +32,20 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{ location, navigate, params }}
+            />
+        );
+    }
+
+    return ComponentWithRouterProp;
+}
+
+export default connect(mapStateToProps, {setUserProfile})(withRouter(ProfileContainer));
